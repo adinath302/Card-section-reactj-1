e0502bdd7bec0bd6451d16a42753ddd7a2588456
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-05-10 23:54:36 +0530

    Add files via upload

---
+++ Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='bg-gray-200 p-20'>
+      <div className='container mx-auto max-w-[500px] text-center'>
+        {/* header section */}
+        <div>
+          <h1>Why Choose Us</h1>
+          <p>benefits of online tutorin services with us</p>
+        </div>
+        {/* card section */}
+      </div>
+    </div>
+  )
+}
+
+export default Cards
